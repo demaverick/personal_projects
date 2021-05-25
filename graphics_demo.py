4219c329ea60044ec86a1ed8d67cf73ddfd265ac
Ananth Deepak Sharma Nanduri <ananth.deepaksharma@gmail.com>
2021-05-25 02:34:37 -0400

    circles in the center of each box

--- graphics_demo.py
+++ graphics_demo.py
@@ -1,7 +1,8 @@
+from typing import Text
 import graphics
 
 def main():
-    win = graphics.GraphWin("My Window", 407,407)
+    win = graphics.GraphWin("2048", 407,407)
     win.setBackground(graphics.color_rgb(200,200,50))
     for j in range(0,400,102):
         for i in range(0,400,102):
@@ -10,7 +11,14 @@ def main():
             print("(",i,",",j,") to (" ,i+j+100,j+100,")")
             sq = graphics.Rectangle(pt, pt2)
             sq.setFill(graphics.color_rgb(0,0,0))
+            center = sq.getCenter()
             sq.draw(win)
+            cir = graphics.Circle(center, 20)
+            cir.setFill(graphics.color_rgb(255,255,255))
+            cir.draw(win)
+            # text = Text(center, "Hello")
+            # text.setTextColor(graphics.color_rgb(255,255,255))
+            # text.draw(win)
     win.getMouse()
     win.close()
 
