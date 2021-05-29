503333ae0050a79f78dc08027a6c58896270e829
Ananth Deepak Sharma Nanduri <ananth.deepaksharma@gmail.com>
2021-05-29 18:50:04 -0400

    Modifying the clear function to undraw Text items only

--- graphics_demo.py
+++ graphics_demo.py
@@ -6,7 +6,9 @@ import random
 
 def clear(win):
     for item in win.items[:]:
-        item.undraw()
+        if "Text" in str(item):
+            item.undraw()
+            print(item," Undrawn")
     win.update()
 
 
@@ -16,26 +18,21 @@ def main():
     inp = "Left"
     board = [0]*16
     while inp in ["Left", "Up", "Down", "Right"]:
-        print("Entere")
         index = 0
         for j in range(0,400,102):
             for i in range(0,400,102):
                 pt = Point(i,j)
                 pt2 = Point(i+100,j+100)
-                print("(",i,",",j,") to (" ,i+j+100,j+100,")")
+                # print("(",i,",",j,") to (" ,i+j+100,j+100,")")
                 sq = Rectangle(pt, pt2)
                 sq.setFill(color_rgb(0,0,0))
                 center = sq.getCenter()
                 sq.draw(win)
-                # cir = Circle(center, 20)
-                # cir.setFill(color_rgb(255,255,255))
-                # cir.draw(win)
                 num = board[index]
-                text = Text(center, str(num))
+                text = Text(center, num)
                 text.setTextColor(color_rgb(255,255,255))
                 text.draw(win)
                 index+=1
-        print("Getting input")
         inp = win.getKey()
         clear(win)
 main()
